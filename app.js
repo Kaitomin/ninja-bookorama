b37fb76d9c2b46de93d159413a8f67b959b62c26
Kaito <hoangdai.d93@gmail.com>
2022-03-10 14:48:45 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const ul = document.querySelector('#book-list ul');
+const addForm = document.forms['add-book'];
+
+
+
+// Add book
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputValue = addForm.querySelector('input').value;
+  const li = document.createElement('li');
+  const nameSpan = document.createElement('span');
+  const deleteSpan = document.createElement('span');
+
+  // Add value
+  nameSpan.textContent = inputValue;
+  deleteSpan.textContent = "delete";
+
+  // Add class
+  nameSpan.classList.add('name');
+  deleteSpan.classList.add('delete');
+
+  li.append(nameSpan, deleteSpan);
+  ul.appendChild(li);
+
+  // Reset value after submit
+  addForm.querySelector('input').value = '';
+
+});
+
+// Delete book
+ul.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (e.target.className == 'delete') {
+        const li = e.target.parentElement;
+        li.remove();
+    }
+});
+
+// Hide all books
+const hideBox = document.querySelector('#hide');
+
+hideBox.addEventListener('change', (e) => {
+    hideBox.checked ? ul.style.display = " none" : ul.style.display = "initial";
+});
+
+// Search book
+const searchBar = document.forms['search-books'].querySelector('input');
+
+document.forms['search-books'].addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchKey = e.target.value.toLowerCase();
+  const books = ul.querySelectorAll('li');
+
+  books.forEach(book =>  {
+    if (!book.querySelector('span.name').textContent.toLowerCase().includes(searchKey)) {
+      book.style.display = "none";
+    } else {
+      book.style.display = "block";
+    }
+  });
+});
+
+// Tabbed content
+const tabs = document.querySelector('.tabs');
+const panel = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', (e) => {
+  if (e.target.tagName == 'LI') {
+    const targetPanel = document.querySelector(e.target.dataset.target);
+
+    panel.forEach(panel => {
+      panel == targetPanel ? panel.classList.add('active') : panel.classList.remove('active');
+    });
+  }
+});
