c9e869e57655ae06ed34d3d4485e12c8e75d3d61
Kaito <hoangdai.d93@gmail.com>
2022-04-08 15:53:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const ul = document.querySelector('#book-list ul');
 const addForm = document.forms['add-book'];
 
 
-
 // Add book
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
